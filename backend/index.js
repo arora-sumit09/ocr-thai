23520993c1982967fb58f934046d1fbd6119fb04
arora-sumit09 <sumitarora1999@gmail.com>
2023-12-26 00:45:34 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,20 @@
-const express=require('express');
-const database = require('./database');
-const cors = require('cors')
+const express = require("express");
+const database = require("./database");
+const cors = require("cors");
 
-require('dotenv').config()
-const PORT= process.env.PORT ||8081;
-var app= express();
-app.use(cors())
-
-
+require("dotenv").config();
+const PORT = process.env.PORT || 8081;
+var app = express();
+app.use(cors());
 
 app.use(express.json());
 
 database();
 
-app.use('/api/', require('./routes/createUser'));
-app.use('/api/', require('./routes/fetchRecord'));
-app.use('/api/', require('./routes/deleteRecord'));
+app.use("/api/", require("./routes/createUser"));
+app.use("/api/", require("./routes/fetchRecord"));
+app.use("/api/", require("./routes/deleteRecord"));
 
 app.listen(PORT, function () {
- 
-    console.log("App listening at http://localhost:",PORT);
- })
- 
+  console.log("App listening at http://localhost:", PORT);
+});
